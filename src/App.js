68a765ab2a8387d3d4231cda364db3563d2eb52c
successfulcode <sergej.mickevic@gmail.com>
2021-06-20 22:09:40 +0300

    i-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './hoc/loyout/Layout';
+import Quiz from './containers/quiz/Quiz';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import QuizList from './containers/quizList/QuizList';
+import QuizCreator from './containers/quizCreator/QuizCreator';
+import Auth from './containers/auth/Auth';
+import { connect } from 'react-redux';
+import Logout from './components/logout/Logout';
+import { autoLogin } from './store/actions/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.autoLogin()
+  }
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/quiz/:id' component={Quiz} />
+        <Route path='/' exact component={QuizList} />
+        <Redirect to='/' />
+      </Switch>
+    )
+    if (this.props.isAutenticated) {
+      routes = (
+        <Switch>
+          <Route path='/quiz-creator' component={QuizCreator} />
+          <Route path='/quiz/:id' component={Quiz} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/' exact component={QuizList} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
+
+    return (
+      <Layout>
+        {routes}
+      </Layout>
+    );
+  }
 }
 
-export default App;
+library.add(fab, faCheckSquare, faCoffee)
+
+function mapStateToProps(state) {
+  return {
+    isAutenticated: !!state.auth.token
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
